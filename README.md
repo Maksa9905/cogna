b904dc1332788b29c251a0185cefb50ae39ffe0c
hakolr <m.gayvoronskiy@flang.io>
2026-02-01 11:37:30 +0300

    initialized and configured project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Cogna
+
+![GitHub Actions Workflow Status](https://github.com/Maksa9905/cogna/actions/workflows/ci/badge.svg)
---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+	plugins: [react()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+			"@entities": path.resolve(__dirname, "./src/entities"),
+			"@features": path.resolve(__dirname, "./src/features"),
+			"@pages": path.resolve(__dirname, "./src/pages"),
+			"@shared": path.resolve(__dirname, "./src/shared"),
+			"@widgets": path.resolve(__dirname, "./src/widgets"),
+			"@app": path.resolve(__dirname, "./src/app"),
+		},
+	},
+});
